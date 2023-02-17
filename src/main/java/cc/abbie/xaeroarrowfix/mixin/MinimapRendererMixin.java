0d35ae6c5f48744a813a01b60eba594c7f642b6c
Abbie <abigail@ient.me>
2023-02-17 18:45:01 +0000

    backport to 1.15.2

--- src/main/java/cc/abbie/xaeroarrowfix/mixin/MinimapRendererMixin.java
+++ src/main/java/cc/abbie/xaeroarrowfix/mixin/MinimapRendererMixin.java
@@ -1,41 +1,31 @@
 package cc.abbie.xaeroarrowfix.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import xaero.common.AXaeroMinimap;
 import xaero.common.XaeroMinimapSession;
-import xaero.common.graphics.CustomVertexConsumers;
 import xaero.common.minimap.MinimapProcessor;
 import xaero.common.minimap.render.MinimapRenderer;
 import xaero.common.settings.ModSettings;
 
 @Mixin(MinimapRenderer.class)
 public abstract class MinimapRendererMixin {
-    @Shadow
+    @Shadow(remap = false)
     protected AXaeroMinimap modMain;
 
     @ModifyVariable(
+            remap = false,
             method = "renderMinimap",
             name = "crosshairDisplayed",
             at = @At(value = "LOAD", ordinal = 1)
     )
-    private boolean xaeroarrowfix$fixMainEntityDot(boolean original, XaeroMinimapSession minimapSession, PoseStack matrixStack, MinimapProcessor minimap, int x, int y, int width, int height, double scale, int size, float partial, CustomVertexConsumers cvc) {
+    private boolean xaeroarrowfix$fixMainEntityDot(boolean original, XaeroMinimapSession minimapSession, MinimapProcessor minimap, int x, int y, int width, int height, double scale, int size, float partial) {
         ModSettings settings = modMain.getSettings();
         int mapSize = minimapSession.getMinimapProcessor().getMinimapSize();
         int shape = settings.minimapShape;
 
         return original || settings.mainEntityAs == 2 || settings.getLockNorth(mapSize / 2, shape);
-    }
-
-    @ModifyVariable(
-            method = "drawArrow",
-            name = "offsetY",
-            at = @At(value = "STORE")
-    )
-    private int xaeroarrowfix$fixArrowOffset(int original) {
-        return -10;
     }
 }
