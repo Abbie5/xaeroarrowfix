b3cd5ff6088e05d2eb1d87d950f05809e7b5e5c7
Abbie <abigail@ient.me>
2023-03-02 09:49:39 +0000

    Port to Forge

---
+++ src/main/java/cc/abbie/xaeroarrowfix/XaeroArrowFix.java
@@ -0,0 +1,5 @@
+package cc.abbie.xaeroarrowfix;
+
+import net.minecraftforge.fml.common.Mod;
+@Mod("xaeroarrowfix")
+public class XaeroArrowFix {}
---
+++ src/main/java/cc/abbie/xaeroarrowfix/mixin/MinimapRendererMixin.java
@@ -0,0 +1,33 @@
+package cc.abbie.xaeroarrowfix.mixin;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import xaero.common.IXaeroMinimap;
+import xaero.common.XaeroMinimapSession;
+import xaero.common.graphics.CustomVertexConsumers;
+import xaero.common.minimap.MinimapProcessor;
+import xaero.common.minimap.render.MinimapRenderer;
+import xaero.common.settings.ModSettings;
+
+@Mixin(MinimapRenderer.class)
+public abstract class MinimapRendererMixin {
+    @Shadow(remap = false)
+    protected IXaeroMinimap modMain;
+
+    @ModifyVariable(
+            remap = false,
+            method = "renderMinimap",
+            name = "crosshairDisplayed",
+            at = @At(value = "LOAD", ordinal = 1)
+    )
+    private boolean xaeroarrowfix$fixMainEntityDot(boolean original, XaeroMinimapSession minimapSession, MatrixStack matrixStack, MinimapProcessor minimap, int x, int y, int width, int height, double scale, int size, float partial, CustomVertexConsumers cvc) {
+        ModSettings settings = modMain.getSettings();
+        int mapSize = minimapSession.getMinimapProcessor().getMinimapSize();
+        int shape = settings.minimapShape;
+
+        return original || settings.mainEntityAs == 2 || settings.getLockNorth(mapSize / 2, shape);
+    }
+}
