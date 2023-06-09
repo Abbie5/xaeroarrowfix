19873839ab13f86ea6abd17280a177041cdb232d
Abbie <abigail@ient.me>
2023-06-09 10:51:35 +0100

    update to 1.20

--- src/main/java/cc/abbie/xaeroarrowfix/mixin/MinimapRendererMixin.java
+++ src/main/java/cc/abbie/xaeroarrowfix/mixin/MinimapRendererMixin.java
@@ -1,33 +1,26 @@
 package cc.abbie.xaeroarrowfix.mixin;
 
-import net.minecraft.client.util.math.MatrixStack;
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import xaero.common.AXaeroMinimap;
-import xaero.common.XaeroMinimapSession;
-import xaero.common.graphics.CustomVertexConsumers;
 import xaero.common.minimap.MinimapProcessor;
+import xaero.common.minimap.radar.MinimapRadar;
+import xaero.common.minimap.render.MinimapFBORenderer;
 import xaero.common.minimap.render.MinimapRenderer;
 import xaero.common.settings.ModSettings;
 
 @Mixin(MinimapRenderer.class)
 public abstract class MinimapRendererMixin {
-    @Shadow(remap = false)
-    protected AXaeroMinimap modMain;
-
-    @ModifyVariable(
+    @WrapWithCondition(
             remap = false,
             method = "renderMinimap",
-            name = "crosshairDisplayed",
-            at = @At(value = "LOAD", ordinal = 1)
+            at = @At(value = "INVOKE", target = "Lxaero/common/minimap/render/MinimapFBORenderer;renderMainEntityDot(Lnet/minecraft/client/gui/GuiGraphics;Lxaero/common/minimap/MinimapProcessor;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/entity/Entity;DDDDFLxaero/common/minimap/radar/MinimapRadar;ZIZZZDLxaero/common/settings/ModSettings;Lnet/minecraft/client/renderer/MultiBufferSource$BufferSource;F)V")
     )
-    private boolean xaeroarrowfix$fixMainEntityDot(boolean original, XaeroMinimapSession minimapSession, MatrixStack matrixStack, MinimapProcessor minimap, int x, int y, int width, int height, double scale, int size, float partial, CustomVertexConsumers cvc) {
-        ModSettings settings = modMain.getSettings();
-        int mapSize = minimapSession.getMinimapProcessor().getMinimapSize();
-        int shape = settings.minimapShape;
-
-        return original || settings.mainEntityAs == 2 || settings.getLockNorth(mapSize / 2, shape);
+    private boolean xaeroarrowfix$fixMainEntityDot(MinimapFBORenderer instance, GuiGraphics guiGraphics, MinimapProcessor minimap, Player p, Entity renderEntity, double ps, double pc, double playerX, double playerZ, float partial, MinimapRadar minimapRadar, boolean lockedNorth, int style, boolean smooth, boolean debug, boolean cave, double dotNameScale, ModSettings settings, MultiBufferSource.BufferSource renderTypeBuffers, float minimapScale) {
+        return !(settings.mainEntityAs == 2 || lockedNorth);
     }
 }
